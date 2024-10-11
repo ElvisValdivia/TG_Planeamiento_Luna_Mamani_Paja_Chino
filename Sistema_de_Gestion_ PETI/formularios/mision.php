d7230f14623dfb9cda6bfc9f8a94d9fb849df612
pieroalexanderppc <pp2020067576@virtual.upt.pe>
2024-10-11 15:25:09 -0500

    Creacion del formulario 1mision

---
+++ Sistema_de_Gestion_ PETI/formularios/mision.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Misión</title>
+    <link rel="stylesheet" href="../css/mision.css">
+</head>
+
+<body>
+    <a class="volver" href="../index.php">INDICE</a>
+    
+    <div class="container">
+        <!-- Título Principal -->
+        <h1>1. MISIÓN</h1>
+
+        <!-- Descripción de la Misión -->
+        <p>La <strong>MISIÓN</strong> es la razón de ser de la empresa/organización.</p>
+        <p>
+            • Debe ser clara, concisa y compartida.<br>
+            • Siempre orientada hacia el cliente no hacia el producto o servicio.<br>
+            • Refleja el propósito fundamental de la empresa en el mercado.
+        </p>
+        <p>
+            En términos generales describe la actividad y razón de ser de la organización y contribuye como una
+            referencia permanente en el proceso de planificación estratégica.
+            <br>Se expresa a través de una oración que define el propósito fundamental de su existencia, estableciendo
+            qué
+            hace la empresa, por qué y para quién lo hace.
+        </p>
+        
+        <div class="ejemplos">
+            <h3>EJEMPLOS</h3>
+            <h3>Empresa de servicios</h3>
+            <h2>La gestión de servicios que contribuyen a la calidad de vida de las personas y generan valor para los
+                grupos de interés.</h2>
+
+            <h3>Empresa productora de café</h3>
+            <h2>
+                Gracias a nuestro entusiasmo, trabajo en equipo y valores, queremos deleitar a todos aquellos que, en el
+                mundo aman la calidad de vida, a través del mejor café que la naturaleza pueda ofrecer, ensalzando los
+                primeros tecnologistas, por la innovación y la imaginación intelectual que nacen de la búsqueda de lo
+                bello en todo lo que hacemos.
+            </h2>
+
+            <h3>Agencia de certificación</h3>
+            <h2>
+                Dar a nuestros clientes ahorro económico y trazabilidad de la gestión de la Calidad, la Salud y la
+                Seguridad, el Medio Ambiente y la Responsabilidad Social de sus productos, servicios, productos y
+                sistemas, obteniendo un resultado que les permita reducir la probabilidad de riesgos y la mejora de los
+                resultados.
+            </h2>
+        </div>
+       
+        <h4>En este apartado describirá la Misión de su empresa.</h4>
+
+        <textarea rows="5" style="width: 100%;"></textarea>
+
+        <div class="info-box">
+            <a class="info-item" href="../index.php">INDICE</a>
+            <a class="info-item" href="../formularios/vision.php">2. VISIÓN</a>
+        </div>
+
+    </div>
+</body>
+
+</html>

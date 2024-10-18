f17426e101719ebf4476eacd0829926addf04df3
unknown <ac2021069830@virtual.upt.pe>
2024-10-18 17:56:46 -0500

    Analisis Part04

--- Sistema_de_Gestion_PETI/Formularios/analisisIE.php
+++ Sistema_de_Gestion_PETI/Formularios/analisisIE.php
@@ -3,41 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <link rel="stylesheet" href="../css/analisis.css">
     <title>Análisis Interno y Externo</title>
-=======
-    <title>Análisis Interno y Externo</title>
-    <link rel="stylesheet" href="../css/analisis.css">
->>>>>>> 7452b4636214ba1537053696c891167375d2de06
 </head>
 <body>
     <div class="container">
         <button class="indice-button">Índice</button>
-<<<<<<< HEAD
         <div class="titulo-container">
             <h1><span class="highlight">5.</span> ANÁLISIS INTERNO Y EXTERNO</h1>
         </div>
         <div class="texto-container">
-            <p>Fijados los objetivos estratégicos se debe analizar las distintas estrategias para lograrlos. De esta forma, 
-                las estrategias son los caminos, vías, o enfoques para alcanzar los objetivos. Responden a la pregunta ¿cómo?. 
-                Para determinar la estrategia, podríamos basarnos en el conjunto de estrategias genéricas y específicas que diferentes profesionales proponen al respecto. 
-                Esta guía, lejos de rozar la teoría, propone llevar a cabo un análisis interno y externo de su empresa para obtener una matriz cruzada e identificar la estrategia 
-                más conveniente a llevar a cabo. Este análisis le permitirá detectar por un lado los factores de éxito (fortalezas y oportunidades), y por otro lado, las debilidades 
-                y amenazas que una empresa debe gestionar.</p>
+            <p>Fijados los objetivos estratégicos se debe analizar las distintas estrategias para lograrlos. De esta forma, las estrategias son los caminos, vías, o enfoques para alcanzar los objetivos. Responden a la pregunta ¿cómo?. Para determinar la estrategia, podríamos basarnos en el conjunto de estrategias genéricas y específicas que diferentes profesionales proponen al respecto. Esta guía, lejos de rozar la teoría, propone llevar a cabo un análisis interno y externo de su empresa para obtener una matriz cruzada e identificar la estrategia más conveniente a llevar a cabo. Este análisis le permitirá detectar por un lado los factores de éxito (fortalezas y oportunidades), y por otro lado, las debilidades y amenazas que una empresa debe gestionar.</p>
         </div>
-        <div class="cuadros-container">
-            <div class="cuadro verde">Análisis Externo</div>
-            <div class="cuadro verde">Análisis Interno</div>
+        <div class="imagen-container">
+            <img src="../images/analisisIE.jpg" alt="Análisis Interno y Externo" class="imagen-analisis">
         </div>
-=======
-        <h1>5. ANÁLISIS INTERNO Y EXTERNO</h1>
-        <p>
-            Fijados los objetivos estratégicos se debe analizar las distintas estrategias para lograrlos. De esta forma, las estrategias son los caminos, vías, o enfoques para alcanzar los objetivos. Responden a la pregunta ¿cómo?. 
-            Para determinar la estrategia, podríamos basarnos en el conjunto de estrategias genéricas y específicas que diferentes profesionales proponen al respecto. Esta guía, lejos de rozar la teoría, propone llevar a cabo un análisis interno y externo de su empresa para obtener una matriz cruzada e identificar la estrategia más conveniente a llevar a cabo. 
-            Este análisis le permitirá detectar por un lado los factores de éxito (fortalezas y oportunidades), y por otro lado, las debilidades y amenazas que una empresa debe gestionar.
-        </p>
->>>>>>> 7452b4636214ba1537053696c891167375d2de06
     </div>
 </body>
 </html>

7452b4636214ba1537053696c891167375d2de06
unknown <ac2021069830@virtual.upt.pe>
2024-10-18 17:32:06 -0500

    Analisis Part02

---
+++ Sistema_de_Gestion_PETI/Formularios/analisisIE.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Análisis Interno y Externo</title>
+    <link rel="stylesheet" href="../css/analisis.css">
+</head>
+<body>
+    <div class="container">
+        <button class="indice-button">Índice</button>
+        <h1>5. ANÁLISIS INTERNO Y EXTERNO</h1>
+        <p>
+            Fijados los objetivos estratégicos se debe analizar las distintas estrategias para lograrlos. De esta forma, las estrategias son los caminos, vías, o enfoques para alcanzar los objetivos. Responden a la pregunta ¿cómo?. 
+            Para determinar la estrategia, podríamos basarnos en el conjunto de estrategias genéricas y específicas que diferentes profesionales proponen al respecto. Esta guía, lejos de rozar la teoría, propone llevar a cabo un análisis interno y externo de su empresa para obtener una matriz cruzada e identificar la estrategia más conveniente a llevar a cabo. 
+            Este análisis le permitirá detectar por un lado los factores de éxito (fortalezas y oportunidades), y por otro lado, las debilidades y amenazas que una empresa debe gestionar.
+        </p>
+    </div>
+</body>
+</html>
